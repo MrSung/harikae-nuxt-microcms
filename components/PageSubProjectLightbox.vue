2b5a30bb6a34fc155415bfc7a6912a0c393dace1
Sungjoon Park <sungjoon512@gmail.com>
2020-04-25 16:28:38 +0900

    refactor: ProjectのLightbox切り出し

---
+++ components/PageSubProjectLightbox.vue
@@ -0,0 +1,164 @@
+<template>
+  <div :class="$style.container">
+    <div :class="$style.containerInner">
+      <div :class="$style.project">
+        <ul :class="$style.projectThumbs">
+          <li
+            v-for="(projectItem, index) in projectGalleryImages"
+            :key="projectItem.id"
+            :class="[
+              $style.projectThumb,
+              currentPathProject.projectModeLandscape &&
+                $style['projectThumb--landscape'],
+              isMounted && $style['projectThumb--mounted'],
+            ]"
+          >
+            <a
+              href="#"
+              :class="[
+                $style.projectThumbLink,
+                isMounted && $style['projectThumbLink--mounted'],
+              ]"
+              @click.prevent="openLightboxOnSlide(index + 1)"
+            >
+              <img
+                ref="galleryImages"
+                v-lazy-load
+                :src="projectGalleryImagesUrl[index]"
+                alt=""
+              />
+            </a>
+          </li>
+        </ul>
+
+        <slot></slot>
+      </div>
+    </div>
+    <FsLightbox
+      :toggler="toggler"
+      :slide="slide"
+      :sources="projectGalleryImagesUrl"
+    />
+  </div>
+</template>
+
+<script>
+import FsLightbox from 'fslightbox-vue'
+import imagesloaded from 'imagesloaded'
+import { nanoid } from 'nanoid'
+
+export default {
+  components: {
+    FsLightbox,
+  },
+  props: {
+    currentPathProject: {
+      type: Object,
+      default: () => [],
+    },
+  },
+  data: () => ({
+    toggler: false,
+    slide: 1,
+    isMounted: false,
+  }),
+  computed: {
+    projectGalleryImages() {
+      return this.currentPathProject.projectGalleryImages.map((obj) => ({
+        ...obj,
+        id: nanoid(),
+      }))
+    },
+    projectGalleryImagesUrl() {
+      return this.projectGalleryImages.map(
+        (image) => image.projectGalleryImage.url
+      )
+    },
+  },
+  mounted() {
+    // Defer the callback to be executed after the next DOM update cycle
+    this.$nextTick(() => {
+      const { galleryImages } = this.$refs
+      // After all gallery images are loaded
+      imagesloaded(galleryImages, () => {
+        this.isMounted = true
+      })
+    })
+  },
+  methods: {
+    openLightboxOnSlide(number) {
+      this.slide = number
+      this.toggler = !this.toggler
+    },
+  },
+}
+</script>
+
+<style lang="scss" module>
+.container {
+  @include mq(sm) {
+    padding: 136px 28px 100px;
+  }
+
+  @include mq(xs) {
+    padding: 84px 16px;
+  }
+}
+
+.containerInner {
+  margin-left: auto;
+  margin-right: auto;
+
+  @include mq(sm) {
+    width: 1180px;
+  }
+}
+
+.projectThumbs {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+}
+
+.projectThumb {
+  @include asr(3, 4);
+
+  overflow-y: hidden;
+
+  @include mq(sm) {
+    width: 236px;
+  }
+
+  @include mq(xs) {
+    width: 33.3333%;
+  }
+
+  &--landscape {
+    @include asr(3, 2);
+
+    @include mq(sm) {
+      width: 295px;
+    }
+
+    @include mq(xs) {
+      width: 50%;
+    }
+  }
+
+  &--mounted::before {
+    background-color: $color-000b47;
+    content: '';
+    display: block;
+  }
+}
+
+.projectThumbLink {
+  &--mounted {
+    transition: opacity 0.2s $trans-timing-func;
+  }
+
+  &--mounted:hover {
+    opacity: 0.1;
+  }
+}
+</style>
--- pages/project/_project.vue
+++ pages/project/_project.vue
@@ -1,62 +1,23 @@
 <template>
-  <div :class="$style.container">
-    <div :class="$style.containerInner">
-      <div :class="$style.project">
-        <ul :class="$style.projectThumbs">
-          <li
-            v-for="(projectItem, index) in projectGalleryImages"
-            :key="projectItem.id"
-            :class="[
-              $style.projectThumb,
-              currentPathProject.projectModeLandscape &&
-                $style['projectThumb--landscape'],
-              isMounted && $style['projectThumb--mounted'],
-            ]"
-          >
-            <a
-              href="#"
-              :class="[
-                $style.projectThumbLink,
-                isMounted && $style['projectThumbLink--mounted'],
-              ]"
-              @click.prevent="openLightboxOnSlide(index + 1)"
-            >
-              <img
-                ref="galleryImages"
-                v-lazy-load
-                :src="projectGalleryImagesUrl[index]"
-                alt=""
-              />
-            </a>
-          </li>
-        </ul>
-        <PageSubProjectArticles
-          :project-heading-en="currentPathProject.projectHeadingEn"
-          :project-content-en="currentPathProject.projectContentEn"
-          :project-heading-ja="currentPathProject.projectHeadingJa"
-          :project-content-ja="currentPathProject.projectContentJa"
-        />
-      </div>
-    </div>
-    <FsLightbox
-      :toggler="toggler"
-      :slide="slide"
-      :sources="projectGalleryImagesUrl"
+  <PageSubProjectLightbox :current-path-project="currentPathProject">
+    <PageSubProjectArticles
+      :project-heading-en="currentPathProject.projectHeadingEn"
+      :project-content-en="currentPathProject.projectContentEn"
+      :project-heading-ja="currentPathProject.projectHeadingJa"
+      :project-content-ja="currentPathProject.projectContentJa"
     />
-  </div>
+  </PageSubProjectLightbox>
 </template>
 
 <script>
-import FsLightbox from 'fslightbox-vue'
-import imagesloaded from 'imagesloaded'
-import { nanoid } from 'nanoid'
 import { API_BASE_URL, API_KEY } from '~/config/microcms'
+import PageSubProjectLightbox from '~/components/PageSubProjectLightbox.vue'
 import PageSubProjectArticles from '~/components/PageSubProjectArticles.vue'
 
 export default {
   components: {
+    PageSubProjectLightbox,
     PageSubProjectArticles,
-    FsLightbox,
   },
   async asyncData({ $axios }) {
     const { contents: projectResponseData } = await $axios.$get(
@@ -67,44 +28,12 @@ export default {
     )
     return { projectResponseData }
   },
-  data: () => ({
-    toggler: false,
-    slide: 1,
-    isMounted: false,
-  }),
   computed: {
     currentPathProject() {
       const { project: projectName } = this.$route.params
       return this.projectResponseData.find(
         (obj) => obj.projectSlug === projectName
       )
-    },
-    projectGalleryImages() {
-      return this.currentPathProject.projectGalleryImages.map((obj) => ({
-        ...obj,
-        id: nanoid(),
-      }))
-    },
-    projectGalleryImagesUrl() {
-      return this.projectGalleryImages.map(
-        (image) => image.projectGalleryImage.url
-      )
-    },
-  },
-  mounted() {
-    // Defer the callback to be executed after the next DOM update cycle
-    this.$nextTick(() => {
-      const { galleryImages } = this.$refs
-      // After all gallery images are loaded
-      imagesloaded(galleryImages, () => {
-        this.isMounted = true
-      })
-    })
-  },
-  methods: {
-    openLightboxOnSlide(number) {
-      this.slide = number
-      this.toggler = !this.toggler
     },
   },
   head: () => ({
@@ -114,72 +43,3 @@ export default {
   }),
 }
 </script>
-
-<style lang="scss" module>
-.container {
-  @include mq(sm) {
-    padding: 136px 28px 100px;
-  }
-
-  @include mq(xs) {
-    padding: 84px 16px;
-  }
-}
-
-.containerInner {
-  margin-left: auto;
-  margin-right: auto;
-
-  @include mq(sm) {
-    width: 1180px;
-  }
-}
-
-.projectThumbs {
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
-}
-
-.projectThumb {
-  @include asr(3, 4);
-
-  overflow-y: hidden;
-
-  @include mq(sm) {
-    width: 236px;
-  }
-
-  @include mq(xs) {
-    width: 33.3333%;
-  }
-
-  &--landscape {
-    @include asr(3, 2);
-
-    @include mq(sm) {
-      width: 295px;
-    }
-
-    @include mq(xs) {
-      width: 50%;
-    }
-  }
-
-  &--mounted::before {
-    background-color: $color-000b47;
-    content: '';
-    display: block;
-  }
-}
-
-.projectThumbLink {
-  &--mounted {
-    transition: opacity 0.2s $trans-timing-func;
-  }
-
-  &--mounted:hover {
-    opacity: 0.1;
-  }
-}
-</style>
